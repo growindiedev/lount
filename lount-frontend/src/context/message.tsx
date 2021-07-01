b79e111132fba9611a6c8e5ee58be4418236fc35
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-07-01 22:09:14 +0530

    added ADD_MESSAGE action to messagesReducer

--- lount-frontend/src/context/message.tsx
+++ lount-frontend/src/context/message.tsx
@@ -1,10 +1,18 @@
 import React, { createContext, useReducer, useContext } from 'react'
 import {getMessages} from '../generated/getMessages'
 //import {getUsers, getUsers_getUsers} from '../generated/getUsers'
+type Message_ = {
+  uuid: string,
+  from: string,
+  to: string,
+  content: string,
+  createdAt: string
+}
 
+//type Action = {type: 'SET_USERS', payload: {users: { username: string, message: Message_, messages: getMessages }}} | {type: 'SET_USER_MESSAGES', payload: {username: string, messages: getMessages}} | {type: 'SET_SELECTED_USER', payload: string} | {type: 'ADD_MESSAGE', payload: any}
+type Action = {type: 'SET_USERS' | 'SET_USER_MESSAGES' | 'SET_SELECTED_USER' | 'ADD_MESSAGE', payload: { username?: string, message?: Message_,  messages?: getMessages}} 
 
-type Action = {type: 'SET_USERS', payload: {users: any}} | {type: 'SET_USER_MESSAGES', payload: {username: string, messages: getMessages}} | {type: 'SET_SELECTED_USER', payload: string}
-type Dispatch = (action: Action) => void
+type Dispatch = (action: Action ) => void
 type AuthProviderProps = {children: React.ReactNode}
 type User = { username: string, messages: getMessages, selected: boolean}
 type State = { users: User[]}
@@ -14,7 +22,10 @@ const MessageDispatchContext = createContext<Dispatch | any>(undefined)
 
 
 const messageReducer = (state: any , action: Action) => {
-  let usersCopy
+  let usersCopy, userIndex
+
+  const { username, message, messages } = action.payload
+
     switch (action.type) {
       case 'SET_USERS':
         
@@ -22,18 +33,18 @@ const messageReducer = (state: any , action: Action) => {
         ...state,
         users: action.payload,
       }
+
     case 'SET_USER_MESSAGES':
-      const { username, messages } = action.payload
+      //const { username, messages } = action.payload
       usersCopy = [...state.users]
-
-      const userIndex = usersCopy.findIndex((u) => u.username === username)
-
+      userIndex = usersCopy.findIndex((u) => u.username === username)
       usersCopy[userIndex] = { ...usersCopy[userIndex], messages }
 
       return {
         ...state,
         users: usersCopy,
       }
+
     case 'SET_SELECTED_USER':
       usersCopy = state.users.map((user: {username: string}) => ({
         ...user,
@@ -44,6 +55,24 @@ const messageReducer = (state: any , action: Action) => {
         ...state,
         users: usersCopy,
       }
+
+    case 'ADD_MESSAGE':
+        usersCopy = [...state.users]
+        userIndex = usersCopy.findIndex((u) => u.username === username)
+  
+        let newUser = {
+          ...usersCopy[userIndex],
+          messages: usersCopy[userIndex].messages
+            ? [message, ...usersCopy[userIndex].messages]
+            : null,
+          latestMessage: message,
+        }
+        usersCopy[userIndex] = newUser
+        return {
+          ...state,
+          users: usersCopy,
+        }
+
     default:
       throw new Error(`Unknown action type`)
   }
